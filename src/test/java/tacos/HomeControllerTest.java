170787002e3c59bec90b25f9e0edf25fd4e5c1f6
Paulpauladq <paultzq@hotmail.com>
2020-12-05 15:42:33 -0800

    test: add tests for design and order page

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -1,13 +1,10 @@
 package tacos;
+
 import static org.hamcrest.Matchers.containsString;
-import static
-        org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static
-        org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static
-        org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static
-        org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,17 +12,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@WebMvcTest   // <1>
 public class HomeControllerTest {
+
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc;   // <2>
+
     @Test
     public void testHomePage() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("home"))
-                .andExpect(content().string(
+        mockMvc.perform(get("/"))    // <3>
+
+                .andExpect(status().isOk())  // <4>
+
+                .andExpect(view().name("home"))  // <5>
+
+                .andExpect(content().string(           // <6>
                         containsString("Welcome to...")));
     }
+
 }
